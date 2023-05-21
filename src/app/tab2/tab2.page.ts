b2fc7352e145662d374fdc4589d78309c84e32dc
Viraj Dhumal <mevirajdhumal@gmail.com>
2023-05-21 12:54:39 +0530

    Added Tab1 and Tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,31 @@ export class Tab2Page {
 
   constructor() {}
 
+  items = [
+    {
+      subject: "Mobile Programming",
+      due: "21/05/2023",
+      labTitle: "SPIT App",
+      labNo: 5,
+      statusOption: "Submitted",
+      statusColor: "success",
+    },
+    {
+      subject: "Process Automation",
+      due: "18/05/2023",
+      labTitle: "JSON Extractor Bot",
+      labNo: 6,
+      statusOption: "Not Submitted",
+      statusColor: "danger",
+    },
+    {
+      subject: "Java Programming",
+      due: "20/05/2023",
+      labTitle: "JPA Connectitvity",
+      labNo: 15,
+      statusOption: "Submitted",
+      statusColor: "success",
+    },
+  ]
+
 }
